49db49a7962b20e20a4f63b6045c8e6071103889
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-07-28 00:06:44 +0200

    Add next project

---
+++ GaussTrick/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+int originalCount = numbers.Count;
+
+for (int i = 0; i < originalCount / 2; i++)
+{
+    numbers[i] += numbers[numbers.Count - 1];
+    numbers.RemoveAt(numbers.Count - 1);
+}
+
+Console.WriteLine(string.Join(" ", numbers));
